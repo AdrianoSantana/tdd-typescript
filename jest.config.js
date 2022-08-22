da68ea64f91bb5132a9c70e08030ceebe3c8482d
AdrianoSantana <>
2022-08-22 07:06:35 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
