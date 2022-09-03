955bd7ea2030ddda5c164d5e2a5cfd4109853a6d
AdrianoSantana <>
2022-09-03 14:57:20 -0300

    test: ensure logErrorRepository receive stackError

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,10 +1,17 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository"
 import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
 
 export class LogControllerDecorator implements Controller {
-  constructor(private readonly controller: Controller) {}
+  constructor(
+    private readonly controller: Controller,
+    private readonly logError: LogErrorRepository
+  ) {}
   
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await this.controller.handle(httpRequest)
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500)
+      this.logError.log(httpResponse.body.stack)
+    return httpResponse
   }
 
 }
