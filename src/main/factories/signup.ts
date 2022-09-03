5396bbad1bdea7d7c8a568edb866e63001b8d19b
AdrianoSantana <>
2022-09-03 08:22:47 -0300

    feat: add logController decorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,14 +2,16 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/sign-up/sign-up";
+import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository )
   const signUpController = new SignUpController(emailValidator, dbAddAccount);
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
