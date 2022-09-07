59a90d560f4ae6fa0d1dab574c163a4417a82fa5
AdrianoSantana <>
2022-09-07 07:46:28 -0300

    feat: should call authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,10 +1,14 @@
+import { Authentication } from "../../../domain/usecases/authentication";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, serverError } from "../../helpers/http-helpers";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { EmailValidator } from "../sign-up/sign-up-protocols";
 
 export class LoginController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator){}
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly authentication: Authentication
+  ){}
   
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -17,6 +21,8 @@ export class LoginController implements Controller {
       const isValidEmail = this.emailValidator.isValid(email)
       if (!isValidEmail)
         return badRequest(new InvalidParamError('email'))
+
+      await this.authentication.auth(email, password)
     } catch (error) {
       return serverError(error)
     }
