8d471f5257585c8c7a70c20567d7c107b9fd10b6
AdrianoSantana <>
2022-09-10 07:56:09 -0300

    test: ensure validation composite returns the first error found it

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,30 +4,38 @@ import { ValidationComposite } from "./validation-composite"
 
 interface SutTypes {
   sut: ValidationComposite,
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 
-const makeValidationStub = (): Validation => {
+const makeValidationStubs = (): Validation[] => {
   class ValidationStub implements Validation {
     validate(input: any): Error {
       return null
     }
   }
-  return new ValidationStub() 
+  return [new ValidationStub() , new ValidationStub() ]
 }
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub()
-  const sut = new ValidationComposite([validationStub])
+  const validationStubs = makeValidationStubs()
+  const sut = new ValidationComposite(validationStubs)
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const validateResult = sut.validate({ field: 'any_value' })
     expect(validateResult).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const validateResult = sut.validate({ field: 'any_value' })
+    expect(validateResult).toEqual(new Error())
   })
 })
