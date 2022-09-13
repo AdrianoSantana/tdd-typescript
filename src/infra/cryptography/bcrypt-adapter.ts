784168d716689011522503d8d3dd0483770619cc
AdrianoSantana <>
2022-09-13 06:57:49 -0300

    feat: should call hash comparer with correct value

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,8 +1,14 @@
 import { Hasher } from "../../data/protocols/criptography/hasher";
 import bcrypt from 'bcrypt'
+import { HashComparer } from "../../data/protocols/criptography/hash-comparer";
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
     constructor(private salt: number) {}
+
+    async comparer(password: string, hashed: string): Promise<boolean> {
+        return await bcrypt.compare(password, hashed)
+    }
+
     async hash(value: string): Promise<string> {
         return await bcrypt.hash(value, this.salt)
     }
